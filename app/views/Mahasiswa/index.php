d96c36b2a2792ed722894aae83e49ad0a9649c46
volumeee <mr.volumee@gmail.com>
2022-09-23 22:35:44 +0700

    insert data

--- app/views/Mahasiswa/index.php
+++ app/views/Mahasiswa/index.php
@@ -1,8 +1,11 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary btn-lg mb-2" data-toggle="modal" data-target="#formModal">
+                Tambah data mahasiswa
+            </button>
             <h1>Daftar Mahasiswa</h1>
-
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
                     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -15,3 +18,58 @@
         </div>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama" name="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="nrp">NRP</label>
+                            <input type="number" class="form-control" id="nrp" name="nrp">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <select class="form-control" id="jurusan" name="jurusan">
+                                <option value="Teknik Informatika">Teknik Informatika</option>
+                                <option value="Teknik Industri">Teknik Industri</option>
+                                <option value="Teknik Mesin">Teknik Mesin</option>
+                                <option value="Teknik Komputer">Teknik Komputer</option>
+                                <option value="Teknik Robotika">Teknik Robotika</option>
+                            </select>
+                        </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    $('#exampleModal').on('show.bs.modal', event => {
+        var button = $(event.relatedTarget);
+        var modal = $(this);
+        // Use above variables to manipulate the DOM
+
+    });
+</script>

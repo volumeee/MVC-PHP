376da35dd023874a529c647a1c58c47e221468d2
volumeee <mr.volumee@gmail.com>
2022-09-24 22:15:33 +0700

    delete data

--- app/views/Mahasiswa/index.php
+++ app/views/Mahasiswa/index.php
@@ -1,10 +1,10 @@
 <div class="container mt-5">
 
-<div class="row">
-    <div class="col-lg-6">
-        <?= Flasher::flash();?>
+    <div class="row">
+        <div class="col-lg-6">
+            <?= Flasher::flash(); ?>
+        </div>
     </div>
-</div>
 
     <div class="row">
         <div class="col-6">
@@ -15,9 +15,10 @@
             <h1>Daftar Mahasiswa</h1>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <li class="list-group-item">
                         <?= $mhs['nama']; ?>
-                        <a href="<?= BASEURL; ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">Detail</a>
+                        <a href="<?= BASEURL; ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-2" onclick="return confirm('yakin dek?');">Hapus</a>
+                        <a href="<?= BASEURL; ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-2">Detail</a>
                     </li>
                 <?php endforeach; ?>
             </ul>
